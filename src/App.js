dc87833f05f468791881de336a0f6011c4d0c6d1
Andrew Brooking <atbrooking.dev@gmail.com>
2019-10-29 14:09:58 -0500

    Added game logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import Jumbotron from './components/Jumbotron/index';
-import Wrapper from './components/Wrapper/index';
+import Game from './components/Game/index';
+import Characters from './images.json';
 
 class App extends React.Component {
 
   state = {
     score: 0,
-    highScore: 0
+    highScore: 0,
+    characters: Characters,
+    clicked: []
   };
 
-  increaseScore() {
+  increaseScore = () => {
     let score = this.state.score + 1;
     let highScore = this.state.highScore;
 
@@ -18,24 +21,59 @@ class App extends React.Component {
       highScore = score;
     }
 
-    this.setState({
-      score,
-      highScore
-    });
+    return {score, highScore};
   }
 
-  reset() {
+  randomizeCharacters = () => {
+    let array = [...this.state.characters]
+    let index = this.state.characters.length;
+    let temp, randIndex;
+
+    while (index !== 0) {
+      randIndex = Math.floor(Math.random() * index);
+      index--;
+
+      temp = array[index];
+      array[index] = array[randIndex];
+      array[randIndex] = temp;
+    }
+
+    return array;
+  }
+
+  charClicked = (id) => {
+    if (this.state.clicked.includes(id)) {
+      this.reset();
+    } else {
+      const clicked = [...this.state.clicked];
+      clicked.push(id);
+      
+      const {score, highScore} = this.increaseScore();
+      const characters = this.randomizeCharacters();
+      
+      this.setState({
+        score,
+        highScore,
+        characters,
+        clicked
+      });
+    }
+  }
+
+  reset = () => {
     this.setState({
       score: 0,
-      highScore: this.state.highScore
+      highScore: this.state.highScore,
+      characters: Characters,
+      clicked: []
     });
   }
 
-  render() {
+  render = () => {
     return (
       <div className="container-fluid">
         <Jumbotron score={this.state.score} highScore={this.state.highScore} />
-        <Wrapper />
+        <Game characters={this.state.characters} charClicked={this.charClicked} />
       </div>
     );
   }
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import characters from '../../images.json';
 
-function Wrapper(props) {
+function Game(props) {
     return (
         <div className="card border rounded shadow bg-white w-75 mx-auto my-3">
             <div className="card-body row">
-                {characters.map((char) => {
+                {props.characters.map((char) => {
                     return (
                     <div className="col" key={char.id}>
                         <div className="card shadow my-3" style={{width: 124, height: 114}}>
-                            <img src={char.image} className="card-img-top" onClick={() => console.log(char.id)} />
+                            <img src={char.image} className="card-img-top" onClick={() => props.charClicked(char.id)} alt="" />
                         </div>
                     </div>
                     )
@@ -19,4 +18,4 @@ function Wrapper(props) {
     );
 }
 
-export default Wrapper;
+export default Game;
