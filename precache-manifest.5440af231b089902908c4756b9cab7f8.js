66069b6ef1d22fcc1692292d42fd4d1cfa19992d
Andrew Brooking <atbrooking.dev@gmail.com>
2019-10-30 09:57:56 -0500

    Updates

---
+++ precache-manifest.5440af231b089902908c4756b9cab7f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396ceb0f1a048c11c6c0ba08b857986b",
+    "url": "/Clicky-Game-React/index.html"
+  },
+  {
+    "revision": "7438f5f17acb700dd36e",
+    "url": "/Clicky-Game-React/static/css/main.1c6ffc02.chunk.css"
+  },
+  {
+    "revision": "b1785d6ba1ada23de819",
+    "url": "/Clicky-Game-React/static/js/2.c680c504.chunk.js"
+  },
+  {
+    "revision": "7438f5f17acb700dd36e",
+    "url": "/Clicky-Game-React/static/js/main.0c8edabc.chunk.js"
+  },
+  {
+    "revision": "a54a837c23d3094463ae",
+    "url": "/Clicky-Game-React/static/js/runtime-main.5e6d6746.js"
+  }
+]);
